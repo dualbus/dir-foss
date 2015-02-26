04d1bfaa67a0cd3c193f35d180bbdb2aedd20205
Eduardo A. Bustamante López <dualbus@gmail.com>
2015-02-26 12:43:38 -0600

    Testing YAML

---
+++ src/build.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import glob
+import pprint
+import sys
+import textwrap
+import yaml
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+def U(k,h):
+    return k in h and h[k] and h[k] is not None
+
+def render_rst(group):
+    print group['nombre']
+    print len(group['nombre'])*'='
+    if U('comentario', group):
+        print
+        print textwrap.fill(group['comentario'])
+    if U('direccion', group):
+        print
+        print u"Dirección"
+        print "---------"
+        pprint.pprint(group['direccion'])
+    if U('contacto', group):
+        print
+        print "Contacto"
+        print "--------"
+        print
+        for k, v in sorted(group['contacto'].iteritems()):
+            print "    * %s: %s" % (k, v)
+    if U('presencia', group):
+        print
+        print "Presencia"
+        print "---------"
+        print
+        for k, v in sorted(group['presencia'].iteritems()):
+            print "    * %s: %s" % (k, v)
+
+    pprint.pprint(group)
+
+for fname in glob.glob('./*.yaml'):
+    with open(fname) as fp:
+        reader = codecs.getreader('utf8')(fp)
+        group = yaml.load(reader)
+        if 'activo' in group and group['activo']:
+            render_rst(group)
